5afc371f6038cf39ce1db753a7504a569bdf4308
Sophie 'Sharky' Schumann <sharky@sharky.pw>
2020-05-20 22:14:28 +0200

    TitleBar needs to be programatically added 🤷‍♀

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,39 @@ use gio::prelude::*;
 
 use glib::clone;
 
-use gtk::{Builder, FileChooserDialog, FileFilter};
+use gtk::{Button, Builder, FileChooserDialog, FileFilter};
 
 use std::env;
+
+#[derive(Default)]
+pub struct Header {
+    container: gtk::HeaderBar
+}
+
+impl Header {
+    pub fn new() -> Self {
+        let container = gtk::HeaderBar::new();
+        container.set_title(Some("Rust GlitchGUI by Sharky"));
+        container.set_show_close_button(true);
+
+        let btn_load = Button::new_from_icon_name(Some("document-new"), gtk::IconSize::Button);
+        btn_load.set_widget_name("btn_load");
+        container.add(&btn_load);
+
+        Header { container }
+    }
+
+    // oh my gaaaaaaaaawd this took entirely too long
+    pub fn get_titlebar_button(&self, name: &str) -> Result<Button, gtk::Widget> {
+        let children = self.container.get_children();
+        for child in children {
+            if child.get_widget_name().unwrap() == name {
+                return child.downcast::<Button>();
+            }
+        }
+        Err(self.container.clone().upcast::<gtk::Widget>())
+    }
+}
 
 fn main() {
     let uiapp = gtk::Application::new(Some("pw.sharky.rust.glitchgui"),
@@ -20,27 +50,53 @@ fn main() {
     uiapp.connect_activate(|app| {
         let builder = Builder::new_from_string(include_str!("main_window.glade"));
 
-        let main_window: gtk::ApplicationWindow = builder.get_object(&"MainWindow").expect("Could not get MainWindow");
+        let main_window: gtk::ApplicationWindow = builder.get_object(&"MainWindow").expect("Could not get MainWindow ?!");
         main_window.set_application(Some(app));
+        main_window.set_resizable(true);
+
+        // Set titlebar for dragging the window around
+        let hdr_bar = Header::new();
+        main_window.set_titlebar(Some(&hdr_bar.container));
         
         
-        let btn_exit: gtk::Button = builder.get_object(&"btn_exit").expect("Could not get btn_exit?");
+        let btn_exit: gtk::Button = builder.get_object(&"btn_exit").expect("Could not get btn_exit?!");
         btn_exit.connect_clicked(clone!(@weak main_window => move |_| {
             // todo: Ask if we are sure?
             main_window.close();
         }));
 
 
-        let btn_load: gtk::Button = builder.get_object(&"btn_load").expect("Could not get btn_load?");
-        btn_load.connect_clicked(clone!(@weak main_window => move |_| {
+        //let btn_load: gtk::Button = builder.get_object(&"btn_load").expect("Could not get btn_load?!");
+        let btn_load = hdr_bar.get_titlebar_button("btn_load").expect("Could not get btn_load?!");
+        btn_load.connect_clicked( move |_| {
             // todo: ask if we want to overwrite the already loaded image?
 
             let fcb = gtk::FileChooserDialogBuilder::new();
-            let _fc: FileChooserDialog = fcb.build();
+            let fc: FileChooserDialog = fcb.build();
+            fc.set_action(gtk::FileChooserAction::Open);
+            fc.add_buttons(&[
+                (&"Open", gtk::ResponseType::Ok), (&"Cancel", gtk::ResponseType::Cancel)
+            ]);
 
-            let _file_filter = FileFilter::new();
+            let file_filter = FileFilter::new();
+            file_filter.add_pattern(&"*.jpg");
+            file_filter.add_pattern(&"*.jpg");
+            fc.set_filter(&file_filter);
+
+            match fc.run() {
+                gtk::ResponseType::Ok => {
+                    // ok, load file
+                    //let source_image = gtk::Image::new_from_file(fc.get_filename().unwrap());
+
+                    let img_image: gtk::Image = builder.get_object(&"img_image").expect("Could not get img_image?!");
+                    img_image.set_from_file(fc.get_filename().unwrap());
+                },
+                gtk::ResponseType::Cancel => return,
+                _ => ()
+            };
+            fc.destroy();
             
-        }));
+        });
 
 
 

8da7b8523c461d9558563896e398224f26cff057
Sophie 'Sharky' Schumann <sharky@sharky.pw>
2020-05-21 19:53:49 +0200

    Cleaning up the arc stuff and experimenting with `set_size_request`

--- src/main.rs
+++ src/main.rs
@@ -107,7 +107,6 @@ fn main() {
     uiapp.connect_activate(|app| {
         let builder: Builder = Builder::new_from_string(include_str!("main_window.glade"));
         let arc_builder = Arc::new(Mutex::new(builder));
-        let arc_builder_c = arc_builder.clone(); // so every owner of a mutex lock gets a clone? coolio?
 
         let main_window: gtk::ApplicationWindow = arc_builder.lock().unwrap().get_object(&"MainWindow").expect("Could not get MainWindow ?!");
         main_window.set_application(Some(app));
@@ -117,7 +116,6 @@ fn main() {
         // Set titlebar for dragging the window around
         let hdr_bar = Header::new();
         let arc_hdr_bar = Arc::new(Mutex::new(hdr_bar));
-        let arc_hdr_bar_c = arc_hdr_bar.clone();
         main_window.set_titlebar(Some(&arc_hdr_bar.lock().unwrap().container));
 
         //The problem: GTK gives you a value in a callback that could be running on a diff thread
@@ -135,42 +133,87 @@ fn main() {
 
         
         let arc_filename = Arc::new(Mutex::new(String::new()));
-        let arc_filename_c = arc_filename.clone();
-        let arc_filename_c2 = arc_filename.clone();
 
-        let btn_load: Button = arc_hdr_bar.lock().unwrap().get_titlebar_button("btn_load").expect("Could not get btn_load?!");
-        btn_load.connect_clicked(move |_| {
-            // todo: ask if we want to overwrite the already loaded image?
+        {
+            let arc_builder = arc_builder.clone();
+            let arc_filename = arc_filename.clone();
+            let arc_hdr_bar = arc_hdr_bar.clone();
+        
+            let btn_load: Button = arc_hdr_bar.lock().unwrap().get_titlebar_button("btn_load").expect("Could not get btn_load?!");
+            btn_load.connect_clicked(move |_| {
+                // todo: ask if we want to overwrite the already loaded image?
 
-            let fcb = gtk::FileChooserDialogBuilder::new();
-            let fc: FileChooserDialog = fcb.build();
-            fc.set_action(gtk::FileChooserAction::Open);
-            fc.add_buttons(&[
-                (&"Open", gtk::ResponseType::Ok),
-                (&"Cancel", gtk::ResponseType::Cancel),
-            ]);
+                let fcb = gtk::FileChooserDialogBuilder::new();
+                let fc: FileChooserDialog = fcb.build();
+                fc.set_action(gtk::FileChooserAction::Open);
+                fc.add_buttons(&[
+                    (&"Open", gtk::ResponseType::Ok),
+                    (&"Cancel", gtk::ResponseType::Cancel),
+                ]);
 
-            let file_filter = FileFilter::new();
-            file_filter.add_pattern(&"*.jpg");
-            file_filter.add_pattern(&"*.jpg");
-            fc.set_filter(&file_filter);
+                let file_filter = FileFilter::new();
+                file_filter.add_pattern(&"*.jpg");
+                file_filter.add_pattern(&"*.jpg");
+                fc.set_filter(&file_filter);
 
-            if let gtk::ResponseType::Ok = fc.run() {
-                // ok, load file
-                *arc_filename_c.lock().unwrap() = fc.get_filename().unwrap().to_str().unwrap().to_string();
+                if let gtk::ResponseType::Ok = fc.run() {
+                    // ok, load file
+                    *arc_filename.lock().unwrap() = fc.get_filename().unwrap().to_str().unwrap().to_string();
 
-                let img_image: gtk::Image = arc_builder.lock().unwrap()
-                    .get_object(&"img_image")
-                    .expect("Could not get img_image?!");
-                let btn_num: gtk::SpinButton = arc_hdr_bar_c.lock().unwrap().get_titlebar_button("btn_num").unwrap();
-                //img_image.set_from_file(fc.get_filename().unwrap());
+                    let img_image: gtk::Image = arc_builder.lock().unwrap()
+                        .get_object(&"img_image")
+                        .expect("Could not get img_image?!");
+                    let btn_num: gtk::SpinButton = arc_hdr_bar.lock().unwrap().get_titlebar_button("btn_num").unwrap();
+                    //img_image.set_from_file(fc.get_filename().unwrap());
 
-                match glitch_imagefile_by_numbytes(&std::path::PathBuf::from(&*arc_filename_c.lock().unwrap()), btn_num.get_value_as_int() as u32) {
+                    match glitch_imagefile_by_numbytes(&std::path::PathBuf::from(&*arc_filename.lock().unwrap()), btn_num.get_value_as_int() as u32) {
+                        Ok(buf) =>  {
+                            img_image.set_from_pixbuf(Some(&buf));
+                            // Resize ApplicationWindow to fit screen (todo: use actual screen dimensions)
+                            let img_alloc = img_image.get_allocation();
+                            let w = img_alloc.width.max(1920);
+                            let h = img_alloc.height.max(1080);
+                            arc_builder.lock().unwrap().get_object::<gtk::ApplicationWindow>("MainWindow").unwrap().
+                                set_size_request(w, h);
+                            img_image.set_size_request(w, h);
+                        },
+                        Err(e) => {
+                            // Show error
+                            let dlg = gtk::MessageDialog::new(
+                                Some(&((arc_builder.lock().unwrap()).get_object::<gtk::ApplicationWindow>("MainWindow").unwrap())),
+                                gtk::DialogFlags::empty(), 
+                                gtk::MessageType::Error,
+                                gtk::ButtonsType::Ok,
+                                &"Could not parse the glitched image, it might be glitched too strongly!"
+                            );
+                            
+                            dlg.run();
+                            dlg.destroy();
+                        }
+                    }
+                };
+                fc.destroy();
+            });
+        }
+
+        { 
+            let arc_builder = arc_builder.clone();
+            let arc_filename = arc_filename.clone();
+
+            let btn_num: gtk::SpinButton = arc_hdr_bar.lock().unwrap().get_titlebar_button("btn_num").expect("Could not get btn_num?!");
+            btn_num.connect_changed(move |me| {
+                let value = me.get_value_as_int();
+                let input_file_name = &*arc_filename.lock().unwrap(); // what is this &* and why does it work lmao
+                if input_file_name.is_empty() { return; }
+
+                let img_image: gtk::Image = arc_builder.lock().unwrap().get_object(&"img_image").expect("Could not get img_image?!");
+
+                match glitch_imagefile_by_numbytes(&std::path::PathBuf::from(input_file_name), value as u32) {
                     Ok(buf) => img_image.set_from_pixbuf(Some(&buf)),
-                    Err(e) => {
+                    Err(_) => {
                         // Show error
                         let dlg = gtk::MessageDialog::new(
-                            Some(&((*arc_builder.lock().unwrap()).get_object::<gtk::ApplicationWindow>("main_window").unwrap())),
+                            Some(&((arc_builder.lock().unwrap()).get_object::<gtk::ApplicationWindow>("MainWindow").unwrap())),
                             gtk::DialogFlags::empty(), 
                             gtk::MessageType::Error,
                             gtk::ButtonsType::Ok,
@@ -181,37 +224,8 @@ fn main() {
                         dlg.destroy();
                     }
                 }
-            };
-            fc.destroy();
-        });
-
-        
-        let btn_num: gtk::SpinButton = arc_hdr_bar.lock().unwrap().get_titlebar_button("btn_num").expect("Could not get btn_num?!");
-        btn_num.connect_changed(move |me| {
-            let value = me.get_value_as_int();
-            let input_file_name = &*arc_filename_c2.lock().unwrap(); // what is this &* and why does it work lmao
-            if input_file_name.is_empty() { return; }
-
-            let img_image: gtk::Image = arc_builder_c.lock().unwrap().get_object(&"img_image").expect("Could not get img_image?!");
-
-            match glitch_imagefile_by_numbytes(&std::path::PathBuf::from(input_file_name), value as u32) {
-                Ok(buf) => img_image.set_from_pixbuf(Some(&buf)),
-                Err(e) => {
-                    // Show error
-                    let dlg = gtk::MessageDialog::new(
-                        Some(&((*arc_builder_c.lock().unwrap()).get_object::<gtk::ApplicationWindow>("main_window").unwrap())),
-                        gtk::DialogFlags::empty(), 
-                        gtk::MessageType::Error,
-                        gtk::ButtonsType::Ok,
-                        &"Could not parse the glitched image, it might be glitched too strongly!"
-                    );
-                    
-                    dlg.run();
-                    dlg.destroy();
-                }
-            }
-        });
-
+            });
+        }
         
 
         main_window.show_all();

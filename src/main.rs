8ccb50c22d916098d6c0033056a0218bb239ae61
Sophie 'Sharky' Schumann <sharky@sharky.pw>
2020-05-21 14:29:14 +0200

    BorrowChecker: FITE ME, me: ok. This is super weird but it kinda works?

--- src/main.rs
+++ src/main.rs
@@ -62,32 +62,40 @@ impl Header {
 fn glitch_imagefile_by_numbytes(
     path: &std::path::PathBuf,
     num_glitches: u32,
-) -> gdk_pixbuf::Pixbuf {
+) -> Result<gdk_pixbuf::Pixbuf, gdk_pixbuf::PixbufError> {
     let mut rng = rand::thread_rng();
     let file = std::fs::File::open(path).unwrap();
     let mut br = std::io::BufReader::new(file);
     let mut bytes: Vec<u8> = Vec::new();
     let _num_bytes = br.read_to_end(&mut bytes).unwrap();
+    let orig_image_bytes = bytes.clone();
 
-    //let orig_image_bytes = bytes.clone(); // for later
 
-    for _ in 0..num_glitches {
-        let glitch_index = (rng.next_u32() as usize % bytes.len()) as usize; 
-        // 💋
-        // no u
-        
-        bytes[glitch_index] = (rng.next_u32() % 256) as u8;
+    let mut tries: u32 = 0;
+    'parse_loop: loop {
+        bytes.clear(); bytes.clone_from(&orig_image_bytes); // reset source image
+
+        for _ in 0..num_glitches {
+            let glitch_index = (rng.next_u32() as usize % bytes.len()) as usize; 
+            // 💋
+            // no u
+            
+            bytes[glitch_index] = (rng.next_u32() % 256) as u8;
+        }
+
+        // Read bytes as MemoryInputStream and try to parse a Pixbuf from it
+        let glib_bytes = glib::Bytes::from(&bytes);
+        let instream = gio::MemoryInputStream::new_from_bytes(&glib_bytes);
+        let not_cancellable: Option<&gio::Cancellable> = None;
+        match gdk_pixbuf::Pixbuf::new_from_stream(&instream, not_cancellable) {
+            Ok(a) => return Ok(a),
+
+            Err(e) => {println!("{:?}", e); if tries > 20 { break 'parse_loop; }}
+        }
+        tries += 1;
     }
-
-    let glib_bytes = glib::Bytes::from_owned(bytes);
-    let instream = gio::MemoryInputStream::new_from_bytes(&glib_bytes);
-
-    let not_cancellable: Option<&gio::Cancellable> = None;
-    match gdk_pixbuf::Pixbuf::new_from_stream(&instream, not_cancellable) {
-        Ok(a) => a,
-
-        Err(e) => panic!(e.to_string()), // todo repeat glitching until parseable
-    }
+    println!("Giving up after 20 tries :(");
+    Err(gdk_pixbuf::PixbufError::Failed)
 }
 
 fn main() {
@@ -98,17 +106,19 @@ fn main() {
     .expect("Application::new failed");
     uiapp.connect_activate(|app| {
         let builder: Builder = Builder::new_from_string(include_str!("main_window.glade"));
-        let builder_c = Arc::new(Mutex::new(builder));
-        let builder_c2 = builder_c.clone(); // so every owner of a mutex lock gets a clone? coolio?
+        let arc_builder = Arc::new(Mutex::new(builder));
+        let arc_builder_c = arc_builder.clone(); // so every owner of a mutex lock gets a clone? coolio?
 
-        let main_window: gtk::ApplicationWindow = builder_c.lock().unwrap().get_object(&"MainWindow").expect("Could not get MainWindow ?!");
+        let main_window: gtk::ApplicationWindow = arc_builder.lock().unwrap().get_object(&"MainWindow").expect("Could not get MainWindow ?!");
         main_window.set_application(Some(app));
         main_window.set_resizable(true);
         main_window.resize(640, 480);
 
         // Set titlebar for dragging the window around
         let hdr_bar = Header::new();
-        main_window.set_titlebar(Some(&hdr_bar.container));
+        let arc_hdr_bar = Arc::new(Mutex::new(hdr_bar));
+        let arc_hdr_bar_c = arc_hdr_bar.clone();
+        main_window.set_titlebar(Some(&arc_hdr_bar.lock().unwrap().container));
 
         //The problem: GTK gives you a value in a callback that could be running on a diff thread
         //Also, as a callback happens *eventually*, the code that happens right after the end of btn_load.connect_clicked
@@ -124,11 +134,11 @@ fn main() {
         //    (the c developer option)  👈
 
         
-        let filename = Arc::new(Mutex::new(String::new()));
-        let filename_c = filename.clone();
-        let filename_c2 = filename.clone();
+        let arc_filename = Arc::new(Mutex::new(String::new()));
+        let arc_filename_c = arc_filename.clone();
+        let arc_filename_c2 = arc_filename.clone();
 
-        let btn_load: Button = hdr_bar.get_titlebar_button("btn_load").expect("Could not get btn_load?!");
+        let btn_load: Button = arc_hdr_bar.lock().unwrap().get_titlebar_button("btn_load").expect("Could not get btn_load?!");
         btn_load.connect_clicked(move |_| {
             // todo: ask if we want to overwrite the already loaded image?
 
@@ -147,25 +157,59 @@ fn main() {
 
             if let gtk::ResponseType::Ok = fc.run() {
                 // ok, load file
-                *filename_c.lock().unwrap() = fc.get_filename().unwrap().to_str().unwrap().to_string();
+                *arc_filename_c.lock().unwrap() = fc.get_filename().unwrap().to_str().unwrap().to_string();
 
-                let img_image: gtk::Image = builder_c.lock().unwrap()
+                let img_image: gtk::Image = arc_builder.lock().unwrap()
                     .get_object(&"img_image")
                     .expect("Could not get img_image?!");
-                img_image.set_from_file(fc.get_filename().unwrap());
+                let btn_num: gtk::SpinButton = arc_hdr_bar_c.lock().unwrap().get_titlebar_button("btn_num").unwrap();
+                //img_image.set_from_file(fc.get_filename().unwrap());
+
+                match glitch_imagefile_by_numbytes(&std::path::PathBuf::from(&*arc_filename_c.lock().unwrap()), btn_num.get_value_as_int() as u32) {
+                    Ok(buf) => img_image.set_from_pixbuf(Some(&buf)),
+                    Err(e) => {
+                        // Show error
+                        let dlg = gtk::MessageDialog::new(
+                            Some(&((*arc_builder.lock().unwrap()).get_object::<gtk::ApplicationWindow>("main_window").unwrap())),
+                            gtk::DialogFlags::empty(), 
+                            gtk::MessageType::Error,
+                            gtk::ButtonsType::Ok,
+                            &"Could not parse the glitched image, it might be glitched too strongly!"
+                        );
+                        
+                        dlg.run();
+                        dlg.destroy();
+                    }
+                }
             };
             fc.destroy();
         });
 
         
-        let btn_num: gtk::SpinButton = hdr_bar.get_titlebar_button("btn_num").expect("Could not get btn_num?!");
+        let btn_num: gtk::SpinButton = arc_hdr_bar.lock().unwrap().get_titlebar_button("btn_num").expect("Could not get btn_num?!");
         btn_num.connect_changed(move |me| {
             let value = me.get_value_as_int();
-            let input_file_name = &*filename_c2.lock().unwrap(); // what is this &* and why does it work lmao
+            let input_file_name = &*arc_filename_c2.lock().unwrap(); // what is this &* and why does it work lmao
+            if input_file_name.is_empty() { return; }
 
-            let glitched_buf = glitch_imagefile_by_numbytes(&std::path::PathBuf::from(input_file_name), value as u32);
-            let img_image: gtk::Image = builder_c2.lock().unwrap().get_object(&"img_image").expect("Could not get img_image?!");
-            img_image.set_from_pixbuf(Some(&glitched_buf));
+            let img_image: gtk::Image = arc_builder_c.lock().unwrap().get_object(&"img_image").expect("Could not get img_image?!");
+
+            match glitch_imagefile_by_numbytes(&std::path::PathBuf::from(input_file_name), value as u32) {
+                Ok(buf) => img_image.set_from_pixbuf(Some(&buf)),
+                Err(e) => {
+                    // Show error
+                    let dlg = gtk::MessageDialog::new(
+                        Some(&((*arc_builder_c.lock().unwrap()).get_object::<gtk::ApplicationWindow>("main_window").unwrap())),
+                        gtk::DialogFlags::empty(), 
+                        gtk::MessageType::Error,
+                        gtk::ButtonsType::Ok,
+                        &"Could not parse the glitched image, it might be glitched too strongly!"
+                    );
+                    
+                    dlg.run();
+                    dlg.destroy();
+                }
+            }
         });
 
         
